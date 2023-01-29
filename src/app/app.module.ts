4fa28075911e82d03fbea00174f094a92b69ab4b
Ong-Jun-Kai <ongjunkai@ecquaria.com>
2023-01-29 23:58:19 +0800

    Create a side navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,6 @@ import { LoginComponent } from './login/login.component';
 import { ErrorComponent } from './error/error.component';
 import { ListTodosComponent } from './list-todos/list-todos.component';
 import { MenuComponent } from './menu/menu.component';
-import { FooterComponent } from './footer/footer.component';
 import { LogoutComponent } from './logout/logout.component';
 import { TodoComponent } from './todo/todo.component';
 
@@ -23,7 +22,6 @@ import { TodoComponent } from './todo/todo.component';
     ErrorComponent,
     ListTodosComponent,
     MenuComponent,
-    FooterComponent,
     LogoutComponent,
     TodoComponent
   ],
